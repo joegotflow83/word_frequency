4f026bf68d1aca03ce7a7c25b42b591883955daa
joegotflow83 <joe@absolutod.com>
2016-02-03 12:57:32 -0500

    Added file of ignored words to disregard in count

--- word_frequency.py
+++ word_frequency.py
@@ -1,12 +1,13 @@
 #!usr/local/env python3.5
 from nltk import word_tokenize
 from nltk.tokenize import RegexpTokenizer
+from collections import defaultdict
 import operator
 
 
-def read_file():
+def read_file(file):
 
-	with open('sample.txt', 'r') as f:
+	with open(file) as f:
 
 		return f.read()
 
@@ -18,18 +19,31 @@ def count_words():
 		tokenizer = RegexpTokenizer(r'\w+')
 		content = tokenizer.tokenize(f.read())
 		words = [word.lower() for word in content]
-		words = sorted(words)
 
 	for word in set(words):
 
-		word_dict[word] = words.count(word)
+		if word in ignored_words():
+
+			continue
+
+		else:
+
+			word_dict[word] = words.count(word)
 
 	return top_20(word_dict)
 
 def top_20(word_dict):
 
-	top_20_words = dict(sorted(word_dict.items(), key=operator.itemgetter(1), reverse=True)[:20])
+	top_20_words = sorted(word_dict.items(), key=operator.itemgetter(1), reverse=True)[:20]
 
 	print(top_20_words)
+
+def ignored_words():
+
+	with open('ignored_words.txt') as f:
+
+		tokenizer = RegexpTokenizer(r'\w+')
+		content = tokenizer.tokenize(f.read())
+		return content
 
 count_words()
