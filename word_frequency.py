494ee48a61edf2ca91af1943b987df17484297e3
joegotflow83 <joe@absolutod.com>
2016-02-03 11:39:01 -0500

    Completed Regular Mode

--- word_frequency.py
+++ word_frequency.py
@@ -1,7 +1,22 @@
 #!usr/local/env python3.5
+from nltk import word_tokenize
 
 
 with open('sample.txt', 'r') as f:
 
-	print(f.read())
-	f.close()
+	#print(f.read())
+	pass
+
+word_dict ={}
+
+with open('sample.txt') as f:
+
+	tokens = word_tokenize(f.read())
+	words = [w.lower() for w in tokens]
+	words = sorted(words)
+
+for word in set(words):
+
+	word_dict[word] = words.count(word)
+
+print(word_dict)
